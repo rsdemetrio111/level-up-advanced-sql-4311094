fa1d382894bdf4ceaccca384ff6b6ffb7561ce05
rsdemetrio111 <rsdemetrio80@hotmail.com>
2024-08-14 16:57:48 +0000

    commit_2

--- Scripts/query.sql
+++ Scripts/query.sql
@@ -1,10 +1,9 @@
-SELECT emp.employeeId, 
-    count(*) AS NumOfCarsSold, 
-    MIN(salesAmount) AS MinSalesAmount, 
-    MAX(salesAmount) AS MaxSalesAmount
-FROM sales sls
-INNER JOIN employee emp
+SELECT emp.employeeId,
+    count(*)AS NumOfCarsSold,
+    MIN(salesAmount) as MinSalesAmt,
+    MAX(salesAmount) as MaxSalesAmt
+FROM sales sls 
+INNER JOIN employee emp 
     ON sls.employeeId = emp.employeeId
 WHERE sls.soldDate >= date('now','start of year')
-GROUP BY emp.employeeId
-HAVING count(*) > 5
+GROUP BY emp.employeeId, emp.firstName, emp.lastName
