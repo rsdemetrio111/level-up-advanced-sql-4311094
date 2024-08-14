77dd9c0e30bac565fc963a792fa1dee373fe2ad2
rsdemetrio111 <rsdemetrio80@hotmail.com>
2024-08-14 19:49:28 +0000

    three

--- Scripts/query.sql
+++ Scripts/query.sql
@@ -1,9 +1,15 @@
-SELECT emp.employeeId,
-    count(*)AS NumOfCarsSold,
-    MIN(salesAmount) as MinSalesAmt,
-    MAX(salesAmount) as MaxSalesAmt
-FROM sales sls 
-INNER JOIN employee emp 
-    ON sls.employeeId = emp.employeeId
-WHERE sls.soldDate >= date('now','start of year')
-GROUP BY emp.employeeId, emp.firstName, emp.lastName
+SELECT emp.firstName, emp.lastName,
+  CASE WHEN strftime('%m', soldDate) = '01'
+    THEN salesAmount END AS JanSales,
+  CASE WHEN strftime('%m', soldDate) = '02'
+    THEN salesAmount END AS FebSales,
+  CASE WHEN strftime('%m', soldDate) = '03'
+    THEN salesAmount END AS MarSales,
+  CASE WHEN strftime('%m', soldDate) = '04'
+    THEN salesAmount END AS AprSales
+FROM sales sls
+INNER JOIN employee emp
+  ON sls.employeeId = emp.employeeId
+WHERE sls.soldDate >= '2021-01-01'
+AND sls.soldDate < '2021-01-01'
+--ORDER BY emp.lastName, emp.firstName
